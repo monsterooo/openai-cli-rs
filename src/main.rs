3ebf01e67d4a44f33569411d971eb08397b3d0b9
monsterooo <createthink.net@gmail.com>
2024-06-21 00:38:20 +0800

    Update actions3

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+use anyhow::Result;
 use clap::Parser;
 use cli::{Cli, Config};
-use dotenv::dotenv;
 use config::Config as Conf;
 use openai::get_client;
-use anyhow::Result;
 use openai_rs::chat::{ChatArguments, Message};
 
 mod cli;
-mod openai;
 mod config;
+mod openai;
 mod openai_rs;
 
 #[tokio::main]
@@ -23,18 +22,19 @@ async fn main() -> Result<()> {
             config.openai_api_key = openai_api_key.clone();
             config.save().unwrap();
             println!("Saving the openai key succeeded.");
-            return Ok(())
-        },
+            return Ok(());
+        }
         None => {}
     }
 
     let client = get_client();
-    let args = ChatArguments::new(cli.model, vec![
-        Message {
+    let args = ChatArguments::new(
+        cli.model,
+        vec![Message {
             role: "user".to_owned(),
             content: cli.question,
-        }
-    ]);
+        }],
+    );
     let res = client.create_chat(args).await.unwrap();
     println!("{}", res);
 
